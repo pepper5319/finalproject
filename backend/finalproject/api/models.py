1e580c38553f6124a48c42e88ae8aca9430f7419
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-01-07 13:44:38 -0500

    added ListrUser class to api/models.py added Recipe and PItem models to api/models.py

--- backend/finalproject/api/models.py
+++ backend/finalproject/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class ListrUser(AbstractUser):
+    def __str__(self):
+        return "{}".format(self.username)
+
+
+class Recipe(models.model):
+    id = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    image_url = models.CharField(max_length=200)
+    ingredients = models.TextField()
+
+
+class PItem(models.model):
+    id = models.CharField(max_length=10)
+    name = models.CharField(max_length=200)
+    date_added = models.DateField(auto_now=True)
+    qty = models.PositiveSmallIntegerField()
+    exp_date = models.DateField()
+    user = models.ForeignKey(ListrUser,
+                             related_name='lists',
+                             on_delete=models.CASCADE)
