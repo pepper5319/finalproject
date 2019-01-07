783e304dc2f2da99caefdf8d5e3465edbcbac47f
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-01-07 10:04:05 -0500

    added env_windows for virtual environment use on Windows

---
+++ backend/env_windows/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\alech\desktop\finalproject\finalproject\backend\env_windows\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
