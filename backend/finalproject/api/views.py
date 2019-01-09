7a2c3a9cc81c228d4ba52a74d7abf9c22ad5b919
Jonathan Alec Hoenigmann <JAHoenigmann@student.fullsail.edu>
2019-01-09 10:54:42 -0500

    added RecipeDetailView, GetPItemsView, PItemDetailView to api/views.py

--- backend/finalproject/api/views.py
+++ backend/finalproject/api/views.py
@@ -4,7 +4,9 @@ from rest_framework import generics, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
+from serializers import PItemSerializer, RecipeSerializer
 # Create your views here.
+
 
 class GetRecipesView(generics.ListCreateAPIView):
     serializer_class = RecipeSerializer
@@ -14,3 +16,33 @@ class GetRecipesView(generics.ListCreateAPIView):
         user = self.request.user
         recipes = Recipe.objects.all()
         return recipes
+
+
+class RecipeDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = RecipeSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+    def get_queryset(self):
+        user = self.request.user
+        recipes = Recipe.objects.all()
+        return recipes
+
+
+class GetPItemsView(generics.ListCreateAPIView):
+    serializer_class = PItemSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+    def get_queryset(self):
+        curUser = self.request.user
+        pItems = PItem.objects.filter(PItem.user = curUser)
+        return pItems
+
+
+class PItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = PItemSerializer
+    permission_classes = (permissions.IsAuthenticated)
+
+    def get_queryset(self):
+        curUser = self.request.user
+        pItems = PItem.objects.filter(PItem.user = curUser)
+        return pItems
