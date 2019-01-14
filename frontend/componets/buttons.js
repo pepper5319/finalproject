b826229a80879ccc313c9b50550883ae88d8d7fa
chico medeiros <grampacupcake@chicos-mbp.students.fullsail.com>
2019-01-14 09:26:36 -0500

    button

---
+++ frontend/componets/buttons.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity} from 'react-native';
+
+const Button = () => {
+    return (
+        <TouchableOpacity>
+            <Text>Placeholder</Text>
+        </TouchableOpacity>
+    )
+}
+
+export { Button };
