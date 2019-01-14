a1d38c6e1eec8834fbc90d26e65e18c7e94fb089
pepper5319 <pepper5319@gmail.com>
2019-01-14 13:26:28 -0500

    Fixed Merging

--- frontend/Main.js
+++ frontend/Main.js
@@ -5,6 +5,8 @@ import { connect } from 'react-redux';
 import {addNumber} from "./redux/actions/mathActions"
 import {subtractNumber} from "./redux/actions/mathActions"
 import { Provider } from 'react-redux';
+import { Button } from './componets/buttons';
+
 //#endregion
 
 //#region Import Components
@@ -17,7 +19,31 @@ class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <View style={[styles.buttoncontainer, styles.titlebar]}>
+          <Button onPress={() => console.log('button pressed!')}>
+            menu
+          </Button>
+          <Text> Home Screen </Text>
+          <Button onPress={() => console.log('button pressed!')}>
+            camera
+          </Button>
+        </View>
+        <View style={[styles.buttoncontainer, styles.restofscreen]}>
+        </View>
+      </View>
+
+      <View style={styles.container}>
+        <View style={[styles.buttoncontainer, styles.titlebar]}>
+          <Button onPress={() => console.log('button pressed!')}>
+            menu
+          </Button>
+          <Text> Home Screen </Text>
+          <Button onPress={() => console.log('button pressed!')}>
+            camera
+          </Button>
+        </View>
+        <View style={[styles.buttoncontainer, styles.restofscreen]}>
+        </View>
       </View>
     );
   }
@@ -27,10 +53,27 @@ class Main extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column'
+  },
+  buttoncontainer:{
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titlebar:{
+    backgroundColor: '#09BD09',
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  restofscreen:{
+    flex: 6,
+    backgroundColor: '#fff'
+  },
+  titletext:{
+    color: 'white',
+    fontSize: 18,
+    fontWeight: '600',
+  }
 });
 
 //#region Map State and Dispatch
