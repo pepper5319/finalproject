fc17dadf9d12166fd7ff9b664950c51e66638f01
JAHoenigmann <JAHoenigmann@student.fullsail.edu>
2019-02-05 10:13:04 -0500

    continued work on script

--- backend/finalproject/api/ingredient_parsing.py
+++ backend/finalproject/api/ingredient_parsing.py
@@ -25,42 +25,77 @@
 #         return saved_value
 #     return None
 
+# def compare_string(str, saved):
+#     curr = ''
+#     i = j = index = 0
+#     for unused in range(0, len(str)):
+#         if curr == saved:
+#             return curr
+#         if i >= len(saved) or index >= len(str):
+#             if curr == saved:
+#                 return curr
+#             else:
+#                 return None
+#         elif str[index] == saved[i]:
+#             # if len(curr) > 0:
+#             #     if curr[-1] == ' ' and saved[i] == ' ':
+#             #         asdf = 0
+#             #     else:
+#             #         curr += saved[i]
+#             # else:
+#             if i <= len(curr):
+#                 if curr[i] !=saved[i]:
+#                     curr += saved[i]
+#
+#             if index + 1 < len(str):
+#                 if str[index + 1] == ' ':
+#                     curr += str[index + 1]
+#                     curr += str[index + 2]
+#                     index += 1
+#                     i += 1
+#                     print(curr)
+#             i += 1
+#             index += 1
+#         else:
+#             i = 0
+#             j += 1
+#             index = j
+#
+#     return None
+
 def compare_string(str, saved):
     curr = ''
     i = j = index = 0
-    for unused in range(0, len(str)):
+    while j < len(str):
+        print(curr)
         if curr == saved:
             return curr
-        if i >= len(saved) or index >= len(str):
-            if curr == saved:
-                return curr
-            else:
-                return None
+        elif i >= len(saved) or index >= len(str):
+            # paranoia check required ?
+            return None
         elif str[index] == saved[i]:
-            # if len(curr) > 0:
-            #     if curr[-1] == ' ' and saved[i] == ' ':
-            #         asdf = 0
-            #     else:
-            #         curr += saved[i]
-            # else:
-            if i <= len(curr):
-                if curr[i] !=saved[i]:
-                    curr += saved[i]
-
-            if index + 1 < len(str):
-                if str[index + 1] == ' ':
-                    curr += str[index + 1]
-                    curr += str[index + 2]
+            if len(curr) >= 2:
+                if curr[-2] == ' ' and curr [-1] == saved[i]:
                     index += 1
                     i += 1
-                    print(curr)
-            i += 1
+                    continue
+            if index + 1 < len(str):
+                if str[index + 1] == ' ':
+                    curr += str[index]
+                    curr += str[index + 1]
+                    curr += str[index + 2]
+                    index += 2
+                    i += 2
+                    continue
+            curr += saved[i]
             index += 1
+            i += 1
         else:
             i = 0
             j += 1
             index = j
 
-    return None
+
 
 print(compare_string('fresh olive oil virgin', 'olive oil'))
+print('test' == 'test ')
