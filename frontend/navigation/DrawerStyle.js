f802afe208aa82a9a164c5bdf0f45a490e23f95e
Jmarsh15 <marshall1515@yahoo.com>
2019-03-06 10:31:04 -0500

    Pull PItems and save to this.state.pantry

--- frontend/navigation/DrawerStyle.js
+++ frontend/navigation/DrawerStyle.js
@@ -12,11 +12,11 @@ export default class DrawerStyle extends React.Component {
       active: '',
     };
 
-    
+
     render() {
       const windowHeight = Dimensions.get("window").height
       const { active } = this.state;
-  
+
       return (
         <Drawer.Section title="MENU"
         style={{backgroundColor:'white', height: windowHeight}}>
@@ -62,4 +62,3 @@ export default class DrawerStyle extends React.Component {
       );
     }
 }
-
