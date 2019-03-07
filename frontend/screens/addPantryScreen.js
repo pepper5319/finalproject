890ac5a985eeefae84015f4ff0ac5c0fde6b3480
pepper5319 <pepper5319@gmail.com>
2019-03-07 10:23:44 -0500

    Base design for new items

--- frontend/screens/addPantryScreen.js
+++ frontend/screens/addPantryScreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, ScrollView, Image, StyleSheet, Text, ListView, Button } from 'react-native';
 import ImagePicker from 'react-native-image-picker';
 import { Drawer } from 'native-base';
+import { TextInput, Card, IconButton, Title, Paragraph } from 'react-native-paper';
 import SideBar from '../navigation/drawerStyle';
 import { navAction } from '../actions/navigationAction.js';
 import { connect } from 'react-redux';
@@ -22,6 +23,16 @@ class AddPantryScreen extends React.Component {
         this.drawer._root.open()
     };
 
+    state = {
+      itemName: '',
+      itemQty: '',
+      itemExp: ''
+    }
+
+    addItem = () => {
+
+    }
+
     render() {
 
         return (
@@ -38,9 +49,41 @@ class AddPantryScreen extends React.Component {
                 <View>
                   <NavbarComp button1={this.openDrawer} button2={this.openDrawer} titleTxt={'Add To Pantry'}/>
                 </View>
-
-                <ScrollView>
-
+                <View style={{padding: 16}}>
+                  <TextInput
+                    style={{marginBottom: 10}}
+                    label='Item Name'
+                    value={this.state.itemName}
+                    mode='outlined'
+                    onChangeText={itemName => this.setState({ itemName })}
+                  />
+                  <View style={{flexDirection: 'row'}}>
+                    <TextInput
+                      keyboardType='numeric'
+                      style={{flex: 1, marginRight: 5}}
+                      label='Item Quantity'
+                      value={this.state.itemQty}
+                      mode='outlined'
+                      onChangeText={itemQty => this.setState({ itemQty })}
+                    />
+                    <TextInput
+                      keyboardType='numeric'
+                      placeholder='MM.DD.YYYY'
+                      style={{flex: 1, marginLeft: 5}}
+                      label='Experation Date'
+                      value={this.state.itemExp}
+                      mode='outlined'
+                      onChangeText={itemExp => this.setState({ itemExp })}
+                    />
+                  </View>
+                </View>
+                <ScrollView style={{}}>
+                  <Card>
+                  <Card.Content right={(props) => <IconButton icon='add'/>}>
+                    <Title>Card title</Title>
+                    <Paragraph>Card content</Paragraph>
+                  </Card.Content>
+                </Card>
                 </ScrollView>
             </Drawer>
         );
