18f4109d3aa206dbdc8aaa3b4b09a39068aa3efd
pepper5319 <pepper5319@gmail.com>
2019-03-13 10:07:18 -0400

    Updated Names

--- frontend/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/UIView+React.d
+++ frontend/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/UIView+React.d
@@ -1,20 +1,10 @@
 dependencies: \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Views/UIView+React.m \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Views/UIView+React.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Views/RCTComponent.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTAssert.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTDefines.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTLog.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTUtils.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Views/RCTShadowView.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Views/RCTLayout.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTRootView.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTBridge.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTBridgeDelegate.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTJavaScriptLoader.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTBridgeModule.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTFrameUpdate.h \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTInvalidating.h
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Views/UIView+React.m \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Views/UIView+React.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Views/RCTComponent.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Base/RCTAssert.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Base/RCTDefines.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Base/RCTLog.h \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Base/RCTUtils.h
--- frontend/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/StaticAnalyzer/React/React/normal/x86_64/RCTCxxConvert.d
+++ frontend/ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/StaticAnalyzer/React/React/normal/x86_64/RCTCxxConvert.d
@@ -1,3 +1,3 @@
 dependencies: \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTCxxConvert.m \
-  /Users/jj/Desktop/Final_Proyect/frontend/node_modules/react-native/React/Base/RCTCxxConvert.h
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Base/RCTCxxConvert.m \
+  /Users/pepper5319/Desktop/Final\ Project/finalproject/frontend/node_modules/react-native/React/Base/RCTCxxConvert.h
