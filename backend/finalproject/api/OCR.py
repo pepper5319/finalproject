62662b08cb1b9f5f896802c85dec2133579d53c6
Jmarsh15 <marshall1515@yahoo.com>
2019-04-09 10:55:14 -0400

    praying OCR works

--- backend/finalproject/api/OCR.py
+++ backend/finalproject/api/OCR.py
@@ -27,12 +27,16 @@ def UPCCodes(recieptID,user):
     for value in num:
         if len(value) == 12:
             #list.append(value)
-            if(UPCDatabase.objects.get(upc=value).exists()):
+            try:
                 code = UPCDatabase.objects.get(upc=value)
                 x = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(10))
                 newItem = PItem.object.create(name=code.name,user = user,static_id=x,qty=1,exp_date=date.today())
                 newItem.save()
-                
+            except UPCDatabase.DoesNotExist:
+                print('Could not find UPC' + value)
+
+
+
             #r = requests.get("https://www.walmart.com/search/?query=" + value)
             #data = r.json()
             temp += '\'upc\': \'' + value +'\','
